1a2d35d0a01aae62d685cd09f6c91fbe441b56c9
René Gielen <rgielen@apache.org>
2019-10-20 22:52:08 +0200

    Add samples module along with SpringBoot skeleton project

---
+++ javafx-weaver-samples/javafx-weaver-springboot-sample/src/main/java/net/rgielen/fxweaver/samples/springboot/JavafxWeaverSpringbootSampleApplication.java
@@ -0,0 +1,13 @@
+package net.rgielen.fxweaver.samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafxWeaverSpringbootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavafxWeaverSpringbootSampleApplication.class, args);
+	}
+
+}
