2f2b0eca3ff389e61ec6a7694477463ab30a6bec
Rene Gielen <rene.gielen@gmail.com>
2019-10-21 10:28:36 +0200

    Fix format

--- samples/springboot-sample/src/main/java/net/rgielen/fxweaver/samples/springboot/JavafxWeaverSpringbootSampleApplication.java
+++ samples/springboot-sample/src/main/java/net/rgielen/fxweaver/samples/springboot/JavafxWeaverSpringbootSampleApplication.java
@@ -11,12 +11,13 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class JavafxWeaverSpringbootSampleApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Application.launch(SpringbootJavaFxApplication.class, args);
-	}
+    }
+
     @Bean
     public FxWeaver fxWeaver(ConfigurableApplicationContext applicationContext) {
-	    // Would also work with javafx-weaver-core only:
+        // Would also work with javafx-weaver-core only:
         // return new FxWeaver(applicationContext::getBean, applicationContext::close);
         return new SpringFxWeaver(applicationContext);
     }
