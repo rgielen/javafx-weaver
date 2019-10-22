f5b607bc981ee3f3f6cc041b072386b1bf31a04c
René Gielen <rgielen@apache.org>
2019-10-22 20:18:56 +0200

    Add full support for FxControllerAndView container.

--- core/src/test/java/net/rgielen/fxweaver/core/FxWeaverTest.java
+++ core/src/test/java/net/rgielen/fxweaver/core/FxWeaverTest.java
@@ -1,6 +1,7 @@
 package net.rgielen.fxweaver.core;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 import net.rgielen.fxweaver.core.support.FxmlViewClassAnnotated;
 import net.rgielen.fxweaver.core.support.FxmlViewClassAnnotatedWithValue;
@@ -12,6 +13,7 @@ import org.junit.Test;
 import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -44,23 +46,32 @@ public class FxWeaverTest {
 
     @Test
     public void loadReturnsControllerWhenNoViewCanBeFound() throws Exception {
-        FxmlViewClassNonAnnotated load = fxWeaver.load(FxmlViewClassNonAnnotated.class, null, null, null);
+        FxControllerAndView<SimpleBean, Node> load = fxWeaver.load(SimpleBean.class, null, null);
         assertThat(load).isNotNull();
+        assertThat(load.getController()).isNotNull().isInstanceOf(SimpleBean.class);
     }
 
     @Test
     public void loadByViewLoadsControllerAndView() throws Exception {
         FXMLLoader fxmlLoader = mock(FXMLLoader.class);
-        fxWeaver.loadByViewUsingFxLoader(fxmlLoader, this.getClass().getResource("foo.fxml"), null, null);
+        fxWeaver.loadByViewUsingFxmlLoader(fxmlLoader, this.getClass().getResource("foo.fxml"), null);
         verify(fxmlLoader).load(any(InputStream.class));
         verify(fxmlLoader).getController();
     }
 
     @Test
-    public void load() throws Exception {
-        FxWeaver.ControllerAndView<SimpleBean, Pane> cav = fxWeaver.load(SimpleBean.class, "/net/rgielen/fxweaver/core/foo.fxml");
+    public void loadControllerAndViewWorksWithValidView() throws Exception {
+        FxControllerAndView<SimpleBean, Pane> cav = fxWeaver.load(SimpleBean.class, "/net/rgielen/fxweaver/core/foo.fxml", null);
         assertThat(cav).isNotNull();
         assertThat(cav.getController()).isNotNull().isInstanceOfAny(SimpleBean.class);
-        assertThat(cav.getView()).isNotNull().isInstanceOfAny(Pane.class);
+        assertThat(cav.getView()).isPresent();
+        assertThat(cav.getView().get()).isInstanceOfAny(Pane.class);
+    }
+
+    @Test
+    public void loadControllerAndViewThrowsExceptionForValidView() throws Exception {
+        assertThatThrownBy(
+                () -> fxWeaver.load(SimpleBean.class, "/net/rgielen/fxweaver/core/notvalid.fxml", null)
+        ).isInstanceOf(FxLoadException.class);
     }
 }
