6077d568cb8668fa7f3c72a943758a13bf26b151
René Gielen <rgielen@apache.org>
2019-10-25 11:38:09 +0200

    Add examples for the direct FxControllerAndView injection feature

--- samples/springboot-sample/src/main/java/net/rgielen/fxweaver/samples/springboot/JavafxWeaverSpringbootSampleApplication.java
+++ samples/springboot-sample/src/main/java/net/rgielen/fxweaver/samples/springboot/JavafxWeaverSpringbootSampleApplication.java
@@ -1,12 +1,18 @@
 package net.rgielen.fxweaver.samples.springboot;
 
 import javafx.application.Application;
+import javafx.scene.Node;
+import net.rgielen.fxweaver.core.FxControllerAndView;
 import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.samples.springboot.application.SpringbootJavaFxApplication;
+import net.rgielen.fxweaver.spring.InjectionPointLazyFxControllerAndViewResolver;
 import net.rgielen.fxweaver.spring.SpringFxWeaver;
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication
 public class JavafxWeaverSpringbootSampleApplication {
@@ -20,6 +26,20 @@ public class JavafxWeaverSpringbootSampleApplication {
         // Would also work with javafx-weaver-core only:
         // return new FxWeaver(applicationContext::getBean, applicationContext::close);
         return new SpringFxWeaver(applicationContext);
+    }
+
+    /**
+     * See {@link net.rgielen.fxweaver.samples.springboot.controller.DialogController#DialogController(FxControllerAndView)}
+     * for an example usage.
+     * <p/>
+     * <strong>MUST be in scope prototype!</strong>
+     */
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public <C, V extends Node> FxControllerAndView<C, V> controllerAndView(FxWeaver fxWeaver,
+                                                                           InjectionPoint injectionPoint) {
+        return new InjectionPointLazyFxControllerAndViewResolver(fxWeaver)
+                .resolve(injectionPoint);
     }
 
 }
