441fae4d5391d629abae2e6be05ff86994a6ebc7
lhanda <62590149+lhanda@users.noreply.github.com>
2021-07-27 18:34:47 +0800

    feat: autoscan and initialize fxmlview annotated beans
    Autoscan basepackages specified in 'fxweaver.autoscan.basepackages' property in application.properties for fxmlview annotated beans and auto initialized them.

--- samples/springboot-starter-sample/src/main/java/net/rgielen/fxweaver/samples/springboot/starter/controller/MainWindow.java
+++ samples/springboot-starter-sample/src/main/java/net/rgielen/fxweaver/samples/springboot/starter/controller/MainWindow.java
@@ -2,28 +2,25 @@ package net.rgielen.fxweaver.samples.springboot.starter.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import net.rgielen.fxweaver.core.FxControllerAndView;
 import net.rgielen.fxweaver.core.FxmlView;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @FxmlView
 public class MainWindow {
 
-    private final FxControllerAndView<SomeDialog, VBox> someDialog;
+    @Autowired
+    private SomeDialog someDialog;
 
     @FXML
     public Button openDialogButton;
 
-    public MainWindow(FxControllerAndView<SomeDialog, VBox> someDialog) {
-        this.someDialog = someDialog;
-    }
-
     @FXML
     public void initialize() {
         openDialogButton.setOnAction(
-                actionEvent -> someDialog.getController().show()
+                actionEvent -> this.someDialog.show()
         );
     }
 
